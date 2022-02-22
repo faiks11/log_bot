9be28b2dbb54f62a43c317fb7cf6b45f73bb0ae8
faiks11 <faiks482@yandex.ru>
2022-02-22 22:03:12 +0300

    Config file

---
+++ config.py
@@ -0,0 +1,6 @@
+settings = {
+    'token': 'OTQ1MDAzMDc4OTM4MTUzMDIx.YhJ0sw.FQqCjTLuNz77LExwqNT2oV_SYl4',  # Enter token here
+    'bot': 'faiks',  # Bot name
+    'id': 0,  # Enter bot ID here (int, not str)
+    'prefix': '!// '  # Default prefix
+}
